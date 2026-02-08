2d5c018a83818fd07eb2754ec3880f4619d829e1
정성찬 <jeonghaejun004@gmail.com>
2026-02-08 23:25:02 +0900

    feat: 기믹 스크립트 보수
    홀드탸일, 몬스터, 부서지는타일 스크립트 보수

--- Assets/Scripts/HoldTileButton.cs
+++ Assets/Scripts/HoldTileButton.cs
@@ -1,54 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class HoldTileButton : MonoBehaviour
 {
     Animator anim;
-    [SerializeField] Transform holdTile;
 
-    [SerializeField] float speed;
+    [SerializeField] HoldTileController controller;
 
-    // 버튼을 눌렀을 시 목표 지점
-    [SerializeField] Vector3 pushPos;
-    Vector3 desPos = Vector3.zero;
-    private void Start()
+    void Start()
     {
-        holdTile.position = Vector3.zero;
         anim = GetComponent<Animator>();
-        StartCoroutine(Move());
     }
 
-    IEnumerator Move()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        while (true)
-        {
-            yield return null;
-            if ((desPos - holdTile.position).magnitude > 0.05f)
-                holdTile.position += (desPos - holdTile.position).normalized * speed * Time.deltaTime;
-            else
-                yield break;
-        }
+        Rigidbody2D rb = collision.attachedRigidbody;
+        if (rb == null) return;
+
+        controller.AddHolder(rb);
+        anim.Play("Push");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.GetComponent<Rigidbody2D>() != null)
-        {
-            desPos = pushPos;
-            StartCoroutine(Move());
-            anim.Play("Push");
-        }
-    }
+        Rigidbody2D rb = collision.attachedRigidbody;
+        if (rb == null) return;
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.GetComponent<Rigidbody2D>() != null)
-        {
-            desPos = Vector3.zero;
-            StartCoroutine(Move());
-            anim.Play("Pull");
-        }
+        controller.RemoveHolder(rb);
+        anim.Play("Pull");
     }
 }
